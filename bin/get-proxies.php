1f3cc940ca475130f182aa40ac3fa624f8c6db57
Dmitry Shabanov <dshabanov@threatstop.com>
2015-11-21 11:18:08 +0300

    #1 added google search

--- bin/get-proxies.php
+++ bin/get-proxies.php
@@ -22,4 +22,31 @@ $service = new \ProxySpider\Service\Spider(
 
 $logger->info('Working on proxyspy');
 $service->gather('http://txt.proxyspy.net/proxy.txt');
+
+$logger->info('Looking for Google results');
+$baseUrl = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.1&q=';
+$searchTerm = 'inurl:txt+%2B:8888+%2B:3128';
+$contents = file_get_contents($baseUrl . $searchTerm);
+$json = json_decode($contents, true);
+if (!empty($json) && $json['responseStatus'] === 200) {
+    $logger->debug("Found ~{$json['responseData']['cursor']['resultCount']} pages");
+    foreach ($json['responseData']['cursor']['pages'] as $page) {
+        $contents = file_get_contents($baseUrl . $searchTerm . '&start=' . $page['start']);
+        $json = json_decode($contents, true);
+
+        if (!empty($json) && $json['responseStatus'] === 200) {
+            foreach ($json['responseData']['results'] as $result) {
+                $logger->debug("Found URL: {$result['url']}");
+                $service->gather($result['url']);
+            }
+        } else {
+            $logger->alert("Can't get search results page. Contents: $contents");
+            break;
+        }
+    }
+} else {
+    $logger->alert("Can't get Google results. Contents: $contents");
+}
+
+
 $logger->info('Done');
